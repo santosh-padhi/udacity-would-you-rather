2092e54f7f665d49449476978c528cf29acd0d9b
santosh <padhisantosh0798@gmail.com>
2020-05-30 13:01:50 +0530

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Grid } from 'semantic-ui-react';
+import { handleInitialData } from './redux/actions/shared';
+import { connect } from 'react-redux';
+import Login from './components/Login';
+import Nav from './components/Nav';
+import Home from './components/Home';
+import UserCard from './components/UserCard';
+import NewPoll from './components/PoolSection/NewPoll';
+import Leaderboard from './components/Leaderboard';
+import NoMatch from './components/NoMatch';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    this.props.handleInitialData();
+  }
+  render() {
+    const { authUser } = this.props;
+    return (
+      <Router>
+        <div className="App">
+          {authUser === null ? (
+            <Route
+              render={() => (
+                <ContentGrid>
+                  <Login />
+                </ContentGrid>
+              )}
+            />
+          ) : (
+            <Fragment>
+              <Nav />
+              <ContentGrid>
+                <Switch>
+                  <Route exact path="/" component={Home} />
+                  <Route path="/questions/bad_id" component={NoMatch} />
+                  <Route path="/questions/:question_id" component={UserCard} />
+                  <Route path="/add" component={NewPoll} />
+                  <Route path="/leaderboard" component={Leaderboard} />
+                  <Route component={NoMatch} />
+                </Switch>
+              </ContentGrid>
+            </Fragment>
+          )}
+        </div>
+      </Router>
+    );
+  }
 }
 
-export default App;
+const ContentGrid = ({ children }) => (
+  <Grid padded="vertically" columns={1} centered>
+    <Grid.Row>
+      <Grid.Column style={{ maxWidth: 550 }}>{children}</Grid.Column>
+    </Grid.Row>
+  </Grid>
+);
+
+function mapStateToProps({ authUser }) {
+  return {
+    authUser
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  { handleInitialData }
+)(App);
